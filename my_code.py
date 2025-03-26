824a0e13371ef8dc0a151a3f4ac93d1199721256
Your Name <cheriehong@utexas.edu>
2025-03-26 16:28:01 -0500

    added new test that should fail

--- my_code.py
+++ my_code.py
@@ -1,6 +1,9 @@
 import pytest
 
+
+  ...
 def fix_phone_num(phone_num_to_fix):
+  
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
@@ -10,5 +13,19 @@ def fix_phone_num(phone_num_to_fix):
   
   return fixed_num
 
+
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  assert fix_phone_num("5554429876") == '(555) 442 9876'
+  assert fix_phone_num("3216543333") == '(321) 654 3333'
+
+
+# this test should fail
+def test_unformatted_num(): 
+    assert fix_phone_num("555-442-98761") == '(555) 442 98761'
+    assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
+
+# def test_raises_error_on_short_params(): 
+#     # Now check that a too short string gives a ValueError
+#   with pytest.raises(ValueError):
+#     fix_phone_num("51")
